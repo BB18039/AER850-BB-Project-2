ec7b34d88c192e10f829f67c0290d6312c2ef03b
BB18039 <bryant.berrio@torontomu.ca>
2025-10-30 18:41:46 -0400

    Finished Part 1 Data Processing
    image generators were set up for the 3 folders and they were used to rescale the pixels and add augmentation, images were loaded into batches of 32 with a size of  (500x500). 1942 training and 431 validation images were found and 3 classes were detected.

--- proj2model.py
+++ proj2model.py
@@ -1,3 +1,36 @@
 #AER8520 Project 2
 #Bryant Berrio 501162030
- 
+#Model Design File 
+
+
+#1 Data Processing
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import models, layers, optimizers, callbacks
+
+basedir=r"C:\Users\bryan\Documents\GitHub\AER850-BB-Project-2\Project 2 Data"
+datadir=os.path.join(basedir, "Data") #file that has train, valid, test folders
+
+traindir=os.path.join(datadir, "train") #train folder
+valdir=os.path.join(datadir, "valid") #valid folder
+testdir=os.path.join(datadir, "test") #test folder
+
+imgsize=(500,500)
+batchsize=32
+seed=42
+
+#Train --> rescale +light augmentation and Val --> rescale only=ImageDataGenerator(rescale only)
+
+train_datagen=ImageDataGenerator(rescale=1./255,shear_range =0.15, zoom_range=0.15,horizontal_flip=True)
+
+val_datagen=ImageDataGenerator(rescale=1./255)
+
+train_gen=train_datagen.flow_from_directory(directory=traindir, target_size=imgsize,batch_size=batchsize, class_mode="categorical", shuffle=True, seed=seed)
+
+val_gen =val_datagen.flow_from_directory(directory=valdir, target_size=imgsize, batch_size=batchsize, class_mode="categorical", shuffle=False)
+
+print("Class indices:", train_gen.class_indices)
+print("Train samples:", train_gen.n, "| Val samples:", val_gen.n)
